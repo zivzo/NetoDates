e0d71d0c233ac71d9069e5f86dac17124d3c0a12
zivzo <zivzorero16@gmail.com>
2022-02-16 09:39:52 +0200

    deleted async function GetHebrewHoliday

--- dates.js
+++ dates.js
@@ -38,31 +38,3 @@ const saveTime = (startDate, endDate) =>{
     //console.log(nDays); 
 
 }
-async function GetHebrewHoliday(){
-    const moadim =await fetch('https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=3448439&M=on&s=on').then(res =>{
-        return res.json()
-    }).catch(err => {
-        console.log(`something went wrong with the fetch request, error: ${err}`);
-        return null;
-    });
-    const israeliHoliday = moadim.items.filter(moad =>{
-        return moad.category == "holiday"// ? moad.country : null;
-    });
-    
-    const mappedHoliday = israeliHoliday.map(holiday => {
-        const ho = {
-            name: holiday.title,
-            date: holiday.date,
-
-          date: new Date(holiday.date),
-            heb: holiday.hebrew
-        }
-        return ho;
-    })
-    console.log(mappedHoliday);
-
-    const sortedHolidays = mappedHoliday.sort((a,b) => {
-        return a.date - b.date;
-    })
-    console.log(sortedHolidays);
-}
