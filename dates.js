b0cc56cab1dd6c1a9e70914fc09dc55a9a565708
zivzo <zivzorero16@gmail.com>
2022-02-16 18:32:59 +0200

    Working fine!

--- dates.js
+++ dates.js
@@ -1,4 +1,5 @@
 // days - 0 sunday...
+//if start day is 6 and end day is 0 we return 1, we should return 0
 
 const saveTime = (startDate, endDate) =>{
     const sDate = new Date(startDate.value);
@@ -8,14 +9,19 @@ const saveTime = (startDate, endDate) =>{
     // console.log(sUtcDay);
     // console.log(eUtcDay);
     const newDateStemp = eDate.getTime() - sDate.getTime();
-    const nDays = (newDateStemp/(1000*60*60*24)+1);//days between two dates
+    const nDays = (newDateStemp/(1000*60*60*24));//days between two dates
     const fullWeeks = Math.floor(nDays/7);
+    let workingDay = nDays - (fullWeeks*2);//NOTE: (-1 in true not include /+1 in false include) to include the start date in the calculation, can remove it if we want to not include the start date
 
-    const workingDay = nDays >= 7 ? nDays - (fullWeeks*2) : eUtcDay-sUtcDay+1;//NOTE: (-1 in true not include /+1 in false include) to include the start date in the calculation, can remove it if we want to not include the start date
+    //NOTE: this If checks if the starting day and the end day are the same and if its saturday or  sunday, if true we substruct one working day {because we include the start day and our algorithm}
+    // if(sUtcDay === 6 || sUtcDay === 0 ||eUtcDay === 6 || eUtcDay === 0 ){
+    //     workingDay--;
+    // }
+    
     
     console.log(`Days between dates ${nDays}`);
     console.log(`Full Weeks: ${fullWeeks}`);
-    console.log(`working days: ${workingDay} from the ${sDate} Included`);
+    console.log(`working days: ${workingDay} from the ${sDate} Excluded`);
     // if(nDays < 7){
     //     const arr = [];
     //     let dayIterate = nDays;
