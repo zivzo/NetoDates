ed94565fc6204a80a9f6ff6fe4a46552969bdaf4
zivzo <zivzorero16@gmail.com>
2022-02-16 18:40:17 +0200

    should work perfectly, no excessive tests yet if full weeks is 0 and sdate is 6 and edate is 0 return 0 {only exception}

--- dates.js
+++ dates.js
@@ -1,7 +1,6 @@
 // days - 0 sunday...
 //if start day is 6 and end day is 0 we return 1, we should return 0
-
-
+//if full weeks is 0 and sdate is 6 and edate is 0 return 0
 const saveTime = (startDate, endDate) =>{
     const sDate = new Date(startDate.value);
     const sUtcDay = sDate.getUTCDay();
@@ -13,7 +12,8 @@ const saveTime = (startDate, endDate) =>{
     const nDays = (newDateStemp/(1000*60*60*24));//days between two dates
     const fullWeeks = Math.floor(nDays/7);
     let workingDay = nDays - (fullWeeks*2);//NOTE: (-1 in true not include /+1 in false include) to include the start date in the calculation, can remove it if we want to not include the start date
-
+    if(fullWeeks === 0 && sUtcDay === 6 && eUtcDay === 0)
+        workingDay = 0;
     //NOTE: this If checks if the starting day and the end day are the same and if its saturday or  sunday, if true we substruct one working day {because we include the start day and our algorithm}
     // if(sUtcDay === 6 || sUtcDay === 0 ||eUtcDay === 6 || eUtcDay === 0 ){
     //     workingDay--;
