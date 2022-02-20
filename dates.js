50f5bf9662d2ed93b1159318484b94999da8e406
zivzo <zivzorero16@gmail.com>
2022-02-20 10:25:50 +0200

    instead of using working day and substruct, we are treating sunday and saturday as thursday

--- dates.js
+++ dates.js
@@ -4,23 +4,32 @@
 const saveTime = (startDate, endDate) =>{
     const sDate = new Date(startDate.value);
     const sUtcDay = sDate.getUTCDay();
-    const eDate = new Date(endDate.value);
+    let eDate = new Date(endDate.value);
     const eUtcDay = eDate.getUTCDay();
     // console.log(sUtcDay);
     // console.log(eUtcDay);
+
+    //NOTE: looking at sunday as thursday
+    if(sUtcDay !== 0 && eUtcDay === 0){
+        eDate.setDate(eDate.getDate() - 2);
+    }
+    //NOTE: looking at saturday as thursday
+    if(sUtcDay !== 0 && eUtcDay === 6 || (sUtcDay === 0 && eUtcDay === 6)){
+        eDate.setDate(eDate.getDate() - 1);
+    }
     const newDateStemp = eDate.getTime() - sDate.getTime();
     const nDays = (newDateStemp/(1000*60*60*24));//days between two dates
     const fullWeeks = Math.floor(nDays/7);
     let workingDay = nDays - (fullWeeks*2);//NOTE: (-1 in true not include /+1 in false include) to include the start date in the calculation, can remove it if we want to not include the start date
+    
     //NOTE: if start date in saturday and end date is sunday, special occasion, working day 0 (algorithem says 1)
-    if(sUtcDay === 6 && eUtcDay === 0)
-    {
-        workingDay--;
-    }
+    // if(sUtcDay === 0 && eUtcDay === 6){
+    //     workingDay--;
+    // }
+    
     //NOTE: if start date in sunday and end date is saturday, special occasion, working day 5 (algorithem says 6)
-    if (sUtcDay===0 &&eUtcDay === 6) {
-        workingDay--;
-    }
+
+    
     //NOTE: this If checks if the starting day and the end day are the same and if its saturday or  sunday, if true we substruct one working day {because we include the start day and our algorithm}
     // if(sUtcDay === 6 || sUtcDay === 0 ||eUtcDay === 6 || eUtcDay === 0 ){
     //     workingDay--;
@@ -28,7 +37,7 @@ const saveTime = (startDate, endDate) =>{
     
     
     console.log(`Days between dates ${nDays}`);
-    console.log(`Full Weeks: ${fullWeeks}`);
+    console.log(`Full Weeks time 2: ${fullWeeks*2}`);
     console.log(`working days: ${workingDay} from the ${sDate} Excluded`);
     // if(nDays < 7){
     //     const arr = [];
